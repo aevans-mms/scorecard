ccd86190d707d890544457c249cf1c6a91ba1759
aevans <aevans@mms.org>
2023-04-25 07:12:34 -0600

    add basic API docs HTML files

--- scorecard-api/src/main/java/scorecard/api/controller/GreetingController.java
+++ scorecard-api/src/main/java/scorecard/api/controller/GreetingController.java
@@ -10,7 +10,14 @@ public class GreetingController
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String getGreeting()
 	{
-		String greeting = "Hi, this is a simple application to manage test cases!";
+		String greeting = "Hi, this is the Scorecard REST API!";
+
+		greeting += """
+			<br>
+			<a href="scorecard.html">Scorecard</a>
+			<br>
+			<a href="testcases.html">Testcases</a>
+				""";
 		return greeting;
 	}
 }
--- scorecard-api/src/main/java/scorecard/api/controller/TestController.java
+++ scorecard-api/src/main/java/scorecard/api/controller/TestController.java
@@ -74,7 +74,4 @@ public class TestController
 
 		return ResponseEntity.ok(testResults);
 	}
-
-
-
 }
